02a7e04aff5042f4da837a5a4d6d67ae06d6c2a1
reza mofidi <35191029+rezapnu@users.noreply.github.com>
2022-06-29 17:37:45 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,9 @@ use App\Http\Controllers\PanelController;
 */
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('panel/logout', [PanelController::class, 'panel_logout']);
+
 
 Auth::routes();
 
-Route::get('/', [PanelController::class, 'index'])->middleware('auth');
 
-Route::group(['middleware' => ['auth'],'prefix' => 'panel'], function () {
-    Route::get('user/list', [PanelController::class, 'userList']);
-    Route::post('user/create', [PanelController::class, 'userCreate']);
-    Route::post('user/update', [PanelController::class, 'userUpdate']);
-    Route::get('user/delete/{id}', [PanelController::class, 'userDelete']);
-
-    Route::get('customer/list', [PanelController::class, 'customerList']);
-    Route::post('customer/create', [PanelController::class, 'customerCreate']);
-    Route::post('customer/update', [PanelController::class, 'customerUpdate']);
-    Route::get('customer/delete/{id}', [PanelController::class, 'customerDelete']);
-
-    Route::get('lottery/new', [PanelController::class, 'lottoryNew']);
-
-});
 
